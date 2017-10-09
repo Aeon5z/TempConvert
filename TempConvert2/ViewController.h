c3be2ad3a116d1819b3a974a767881b947e62fda
Aeon5z <32336091+Aeon5z@users.noreply.github.com>
2017-10-09 08:56:17 -0700

    Initial Commit

---
+++ TempConvert2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TempConvert2
+//
+//  Created by Aeonz on 10/9/17.
+//  Copyright © 2017 Aeonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TempConvert2Tests/TempConvert2Tests.m
@@ -0,0 +1,39 @@
+//
+//  TempConvert2Tests.m
+//  TempConvert2Tests
+//
+//  Created by Aeonz on 10/9/17.
+//  Copyright © 2017 Aeonz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TempConvert2Tests : XCTestCase
+
+@end
+
+@implementation TempConvert2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
